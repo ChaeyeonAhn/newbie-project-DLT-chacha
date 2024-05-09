9d6bc20a3f297b66820da6773aec789b6e8d8368
chacha <chacha@sparcs.org>
2024-05-09 23:04:14 +0900

    Trying posts chacha

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import HomePage from '/pages/Home.tsx';
+import PostPage from '/pages/Post.tsx';
+
+import './App.css';
+
+/*
+ Router : BrowserRouter, 라우팅이 작동하는 환경
+ Routes : 다양한 라우트를 담는 것
+ Route : 어떤 경로의 URL 에서는 어떤 컴포넌트로 연결해준다. 하는 것
+*/
+const App = () => {
+  return (
+    <Router> 
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/post/:id" element={<PostPage />} /> 
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React from 'react'; 
+import useEffect from 'react'; /* 외부 API 와 교류 */
+import useState from 'react';
+
+/* 여기다가 지금까지 쓴 post 들을 띄우고 싶으니, 백에서 그들을 가져오는 axios가 필요하다. */
+import axios from 'axios';
+
