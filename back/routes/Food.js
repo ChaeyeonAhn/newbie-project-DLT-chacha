6b65b1af94ded6fdfd5448331c2d5bc4f15cb085
chacha <chacha@sparcs.org>
2024-05-16 12:15:04 +0000

    Food diary

--- back/routes/Food.js
+++ back/routes/Food.js
@@ -7,10 +7,12 @@ const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+/* 회원의 나이, 성별 값 디비에서 가져오기 */
 
 router.get('/:username/member-info', async (req, res) => {
   const { username, date } = req.params;
   const username_fixed = username.replace(/^:+|:+$/g, '');
+  
   try {
     const getInfo = await prisma.member.findMany({
       where: {
@@ -28,5 +30,35 @@ router.get('/:username/member-info', async (req, res) => {
     res.status(400).json({message: `error: ${e}`});
   }
 });
+
+router.post('/:username/:date/save', async (req, res) => {
+  const { username, date } = req.params;
+  const { height, weight, totalCalorie } = req.body;
+  const username_fixed = username.replace(/^:+|:+$/g, '');
+  const dateCode = username_fixed.concat(date);
+  console.log(username_fixed);
+  
+  try {
+    const saveInfo = await prisma.diet.update({
+      data: {
+        height: height,
+        weight: weight,
+        totalCalorie: totalCalorie
+      },
+      where: {
+        dateCode: dateCode
+      }
+    });
+    res.status(200).json({message: 'Saved!'});
+  }
+  catch (e) {
+    res.status(400).json({message: `error: ${e}`});
+  }
+
+});
+
+/* 회원의 그 날의 키, 몸무게, 토탈 섭취 권장 칼로리 입력 */
+
+
 
 module.exports = router;
--- client/src/pages/Food.tsx
+++ client/src/pages/Food.tsx
@@ -19,8 +19,6 @@ const Food = () => {
   const [SBirth, setSBirth] = useState("");
   const [SAge, setSAge] = useState(0); /* 나이 추가 계산 해야 함! */
   
-  
-
   useEffect(() => {
     const getInfo = async () => {
       const { data }= await axios.get(`http://localhost:8000/food/:${username}/member-info`);
@@ -29,51 +27,54 @@ const Food = () => {
       setSGender(data[0].gender);
       setSBirth(data[0].birth); /* 나이는 우리가 직접 계산해서 입력해주기. 그래야 사용자가 나이를 늘 업데이트 하지 않아도 됨 */
 
-      if (data[0].gender === 'f') {
-        setSTotalCalorie( 10 * SWeight + 6.25 * SHeight + 5 * SAge - 161 );
-      }
-      else {
-        setSTotalCalorie( 10 * SWeight + 6.25 * SHeight + 5 * SAge + 5 );
-      }
-
     }
     getInfo().catch((e) => window.alert(`Error while Running API Call: ${e}`));
   });
 
-  const sendPost = () => {
+  const SaveHW = () => {
+    const today = new Date();
+
+    const age = (today.getFullYear() - SBirth.slice(0,4));
+    
+    setSAge(age);
+    console.log(age);
+
+    if (SGender === 'f') {
+      setSTotalCalorie( 10 * SWeight + 6.25 * SHeight + 5 * SAge - 161 );
+    }
+    else {
+      setSTotalCalorie( 10 * SWeight + 6.25 * SHeight + 5 * SAge + 5 );
+    }
+
     const asyncFun = async () => {
-      const { data } = await axios.post(`http://localhost:8000/posts/:${username}/:${date}/update-schedule`, {
-        username: username,
-        date: date,
-        amTime1: SAMTime1,
-        amContent1: SAMContent1,
-        amTime2: SAMTime2,
-        amContent2: SAMContent2,
-        amTime3: SAMTime3,
-        amContent3: SAMContent3,
-        pmTime1: SPMTime1,
-        pmContent1: SPMContent1,
-        pmTime2: SPMTime2,
-        pmContent2: SPMContent2,
-        pmTime3: SPMTime3,
-        pmContent3: SPMContent3,
-        pmTime4: SPMTime4,
-        pmContent4: SPMContent4,
-        pmTime5: SPMTime5,
-        pmContent5: SPMContent5
-      });
-      console.log(data);
-      window.alert(`Successfully Modified!`);
-      setSChange(!SChange);
+      /* 나이는 굳이 db 에 저장 안 해도 될 듯 */
+      console.log(SHeight, SWeight, STotalCalorie); /* STotalCalorie 바로 반영 안 됨 */
+
+      if (SGender === 'f') {
+        const totCal = 10 * SWeight + 6.25 * SHeight + 5 * SAge - 161;
+        await axios.post(`http://localhost:8000/food/:${username}/:${date}/save`, {
+          height: SHeight,
+          weight: SWeight,
+          totalCalorie: totCal
+        });
+        window.alert(`Saved!`);
+        setSChange(!SChange);
+      }
+
+      else {
+        const totCal = 10 * SWeight + 6.25 * SHeight + 5 * SAge + 5;
+        await axios.post(`http://localhost:8000/food/:${username}/:${date}/save`, {
+          height: SHeight,
+          weight: SWeight,
+          totalCalorie: totCal
+        });
+        window.alert(`Saved!`);
+        setSChange(!SChange);
+      }
+
     }
     asyncFun().catch((e) => window.alert(`ERROR: ${e}`));
   };
-
-  const SaveHW = () => {
-    const asyncFun = async () => {
-
-    }
-  }
 
 
 
@@ -82,7 +83,7 @@ const Food = () => {
       <div>
         키 <input type="number" value={SHeight} onChange={(e) => setSHeight(e.target.value)}/>
         체중 <input type="number" value={SWeight} onChange={(e) => setSWeight(e.target.value)}/>
-        <button onClick={SaveHW()}>Confirm</button>
+        <button onClick={() => SaveHW()}>Confirm</button>
         권장 소비 칼로리 <p>{STotalCalorie}</p>
       </div>
     </div>
