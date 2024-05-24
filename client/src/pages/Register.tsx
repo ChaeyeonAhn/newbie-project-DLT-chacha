1166cab1311f24e59f845d4e12c217969f462816
chacha <chacha@sparcs.org>
2024-05-24 09:31:33 +0000

    final..

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -22,7 +22,7 @@ const Register = ({ pop, close }) => {
     if (SFemale) {
       const gender = 'f';
       const asyncFun = async () => {
-        const { data } = await axios.post("https://api.chacha.newbie.sparcsandbox.com/register/new", {
+        await axios.post("https://api.chacha.newbie.sparcsandbox.com/register/new", {
           nickname: SNickname,
           password: SPassword,
           gender: gender,
@@ -41,7 +41,7 @@ const Register = ({ pop, close }) => {
     else if (SMale) {
       const gender = 'm';
       const asyncFun = async () => {
-        const { data } = await axios.post("https://api.chacha.newbie.sparcsandbox.com/register/new", {
+        await axios.post("https://api.chacha.newbie.sparcsandbox.com/register/new", {
           nickname: SNickname,
           password: SPassword,
           gender: gender,
@@ -71,7 +71,6 @@ const Register = ({ pop, close }) => {
         <div id="titlebox">
           <p id="title">Sign Up</p>
         </div>
-        <div>
           <div className="labels">ID</div>
           <input className="nickname" type="text" label="ID" value={SNickname} onChange={e => setSNickname(e.target.value)} placeholder="Enter SPARCS nickname"/>
           <div className="labels">PW</div>
@@ -83,7 +82,6 @@ const Register = ({ pop, close }) => {
           </div>
           <div className="labels">생년월일</div>
           <input className="password" type="date" value={SBirthDate} onChange={e => setSBirthDate(e.target.value)} placeholder="Enter Birth Date"/>
-        </div>
         <div id="regisButton">
           <button id="confirm" onClick={(e) => sendRegister()}><FontAwesomeIcon icon={faCheck} /></button>
           <button id ="close" onClick={close}><FontAwesomeIcon icon={faXmark} /></button>
